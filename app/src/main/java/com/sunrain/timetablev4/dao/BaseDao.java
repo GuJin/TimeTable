7c4d7b25c9679cb9f44b08805a0cc25899899b16
gujin <407185726@qq.com>
2018-01-24 23:45:29 +0800

    访问权限修改

--- app/src/main/java/com/sunrain/timetablev4/dao/BaseDao.java
+++ app/src/main/java/com/sunrain/timetablev4/dao/BaseDao.java
@@ -14,7 +14,7 @@ class BaseDao {
      * @param tableName     要操作的表名
      * @param contentValues 表中个字段的键值对（键的名字要和表中字段名一致，区分大小写）
      */
-    protected static void insert(SQLiteDatabase db, String tableName, ContentValues contentValues) {
+    static void insert(SQLiteDatabase db, String tableName, ContentValues contentValues) {
         db.insert(tableName, null, contentValues);
     }
 
@@ -28,25 +28,23 @@ class BaseDao {
      * @param tableName     要操作的表名
      * @param contentValues 表中个字段的键值对（键的名字要和表中字段名一致，区分大小写）
      */
-    protected static void insertOrReplace(SQLiteDatabase db, String tableName, ContentValues contentValues) {
+    static void insertOrReplace(SQLiteDatabase db, String tableName, ContentValues contentValues) {
         db.insertWithOnConflict(tableName, null, contentValues, SQLiteDatabase.CONFLICT_REPLACE);
     }
 
     /**
      * 删除
-     * <p/>
-     * 例子：db.delete("students","appName=? and id=?",new String[]{"Jake", "2"});
      *
      * @param db          数据库
      * @param tableName   要操作的表名
      * @param whereClause where条件
      * @param whereArgs   where条件语句中的字段值
      */
-    protected static void delete(SQLiteDatabase db, String tableName, String whereClause, String[] whereArgs) {
+    static void delete(SQLiteDatabase db, String tableName, String whereClause, String[] whereArgs) {
         db.delete(tableName, whereClause, whereArgs);
     }
 
-    protected static void update(SQLiteDatabase db, String tableName, ContentValues values, String whereClause, String[] whereArgs) {
+    static void update(SQLiteDatabase db, String tableName, ContentValues values, String whereClause, String[] whereArgs) {
         db.update(tableName, values, whereClause, whereArgs);
     }
 
@@ -58,7 +56,7 @@ class BaseDao {
      * @param selection     条件语句，where部分
      * @param selectionArgs where语句中的字段值
      */
-    protected static Cursor query(SQLiteDatabase db, String tableName, String selection, String[] selectionArgs) {
+    static Cursor query(SQLiteDatabase db, String tableName, String selection, String[] selectionArgs) {
         return queryComplex(db, tableName, null, selection, selectionArgs, null, null, null, null);
     }
 
@@ -75,8 +73,8 @@ class BaseDao {
      * @param orderBy       SQL中的排序语句
      * @param limit         限制返回的数据与偏移量
      */
-    protected static Cursor queryComplex(SQLiteDatabase db, String tableName, String[] columns, String selection, String[] selectionArgs,
-                                         String groupBy, String having, String orderBy, String limit) {
+    static Cursor queryComplex(SQLiteDatabase db, String tableName, String[] columns, String selection, String[] selectionArgs, String
+            groupBy, String having, String orderBy, String limit) {
         return db.query(tableName, columns, selection, selectionArgs, groupBy, having, orderBy, limit);
     }
 }
