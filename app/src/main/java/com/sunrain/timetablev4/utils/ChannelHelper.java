0ebee95a14ce9f921cd19255365d591e869851cf
gujin <407185726@qq.com>
2018-01-26 01:40:52 +0800

    临时移除walle

--- app/src/main/java/com/sunrain/timetablev4/utils/ChannelHelper.java
+++ app/src/main/java/com/sunrain/timetablev4/utils/ChannelHelper.java
@@ -1,18 +1,17 @@
 package com.sunrain.timetablev4.utils;
 
-import com.meituan.android.walle.WalleChannelReader;
 import com.sunrain.timetablev4.BuildConfig;
-import com.sunrain.timetablev4.application.MyApplication;
 
 public class ChannelHelper {
 
     private static String sChannel;
 
     static {
+        sChannel = "test";
         if (BuildConfig.DEBUG) {
             sChannel = "test";
         } else {
-            sChannel = WalleChannelReader.getChannel(MyApplication.sContext, "unknown");
+            //sChannel = WalleChannelReader.getChannel(MyApplication.sContext, "unknown");
         }
     }
 
