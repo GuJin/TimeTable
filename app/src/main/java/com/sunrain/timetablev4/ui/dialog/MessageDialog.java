8eedfc68bdee52831fe409c59c3127584b5e0960
gujin <407185726@qq.com>
2018-03-01 00:35:30 +0800

    限制最大高度

--- app/src/main/java/com/sunrain/timetablev4/ui/dialog/MessageDialog.java
+++ app/src/main/java/com/sunrain/timetablev4/ui/dialog/MessageDialog.java
@@ -3,15 +3,25 @@ package com.sunrain.timetablev4.ui.dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
+import android.text.method.ScrollingMovementMethod;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.sunrain.timetablev4.R;
+import com.sunrain.timetablev4.application.MyApplication;
 import com.sunrain.timetablev4.base.BaseDialog;
 
 public class MessageDialog extends BaseDialog<MessageDialog> {
+
+    private static final int MAX_HEIGHT;
+
+    static {
+        DisplayMetrics displayMetrics = MyApplication.sContext.getResources().getDisplayMetrics();
+        MAX_HEIGHT = (int) (displayMetrics.heightPixels * 0.6);
+    }
 
     private TextView mTextView;
 
@@ -23,6 +33,8 @@ public class MessageDialog extends BaseDialog<MessageDialog> {
     protected View getContentView(Context context, ViewGroup parent) {
         View view = LayoutInflater.from(context).inflate(R.layout.dialog_message, parent, false);
         mTextView = view.findViewById(R.id.tv_message);
+        mTextView.setMaxHeight(MAX_HEIGHT);
+        mTextView.setMovementMethod(ScrollingMovementMethod.getInstance());
         return view;
     }
 
