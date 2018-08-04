f5c04a4dc1410f8bf5baa941877ad0065f2d1c07
GuJin <gujin6@qq.com>
2018-08-04 15:01:42 +0800

    替换Thread为线程池

--- app/src/main/java/com/sunrain/timetablev4/manager/WallpaperManager.java
+++ app/src/main/java/com/sunrain/timetablev4/manager/WallpaperManager.java
@@ -2,15 +2,18 @@ package com.sunrain.timetablev4.manager;
 
 import android.app.Activity;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
 import com.sunrain.timetablev4.R;
 import com.sunrain.timetablev4.application.MyApplication;
+import com.sunrain.timetablev4.utils.RunnableExecutorService;
 
 import java.io.File;
 import java.lang.ref.WeakReference;
+import java.util.concurrent.Callable;
 
 import tech.gujin.toast.ToastUtil;
 
@@ -20,34 +23,34 @@ public class WallpaperManager {
 
     public void refreshWallpaperInBackground(Activity activity) {
         final WeakReference<Activity> activityWeakReference = new WeakReference<>(activity);
-        new Thread(new Runnable() {
+        RunnableExecutorService.when(new Callable<Drawable>() {
             @Override
-            public void run() {
-                final Drawable drawable;
+            public Drawable call() {
                 File picFile = new File(MyApplication.sContext.getFilesDir(), FILE_NAME);
                 Resources resources = MyApplication.sContext.getResources();
-
-                if (!picFile.exists()) {
-                    drawable = new BitmapDrawable(resources, BitmapFactory.decodeResource(resources, R.drawable.bg_wallpaper_default));
+                Bitmap bitmap;
+                if (picFile.exists()) {
+                    bitmap = BitmapFactory.decodeFile(picFile.getAbsolutePath());
                 } else {
-                    drawable = new BitmapDrawable(resources, BitmapFactory.decodeFile(picFile.getAbsolutePath()));
+                    bitmap = BitmapFactory.decodeResource(resources, R.drawable.bg_wallpaper_default);
                 }
-
-                final Activity weakActivity = activityWeakReference.get();
+                return new BitmapDrawable(resources, bitmap);
+            }
+        }).done(new RunnableExecutorService.DoneCallback<Drawable>() {
+            @Override
+            public void done(Drawable drawable) {
+                Activity weakActivity = activityWeakReference.get();
                 if (weakActivity == null) {
-                    ToastUtil.postShow("壁纸加载失败");
                     return;
                 }
-
-                weakActivity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        weakActivity.getWindow().setBackgroundDrawable(drawable);
-                    }
-                });
-
+                weakActivity.getWindow().setBackgroundDrawable(drawable);
             }
-        }).start();
+        }).fail(new RunnableExecutorService.FailCallback() {
+            @Override
+            public void fail(Throwable throwable) {
+                ToastUtil.show("壁纸加载失败");
+            }
+        }).execute();
     }
 
     public static WallpaperManager getInstance() {
