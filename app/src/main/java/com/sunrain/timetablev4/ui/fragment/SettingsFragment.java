e622d0b8182adca3b874cfdbcab7958e565561e1
GuJin <gujin6@qq.com>
2018-11-28 00:46:52 +0800

    修复fragment重叠的问题

--- app/src/main/java/com/sunrain/timetablev4/ui/fragment/SettingsFragment.java
+++ app/src/main/java/com/sunrain/timetablev4/ui/fragment/SettingsFragment.java
@@ -23,9 +23,19 @@ import com.sunrain.timetablev4.ui.fragment.settings.TableFragment;
 
 public class SettingsFragment extends BaseFragment implements RadioGroup.OnCheckedChangeListener, CompoundButton.OnCheckedChangeListener {
 
-    private RadioGroup rgSettings;
+    private RadioGroup mRgSettings;
+    private RadioButton mRbSemester;
     private FragmentChanger mFragmentChanger;
     private SparseArray<Class<? extends Fragment>> mFragmentArray;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            mFragmentChanger = new FragmentChanger(getChildFragmentManager(), R.id.fl_content);
+            mFragmentChanger.onRestoreInstanceState(savedInstanceState);
+        }
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View createView(LayoutInflater inflater, ViewGroup container) {
@@ -34,30 +44,36 @@ public class SettingsFragment extends BaseFragment implements RadioGroup.OnCheck
 
     @Override
     protected void initView(View view, @Nullable Bundle savedInstanceState) {
-        rgSettings = view.findViewById(R.id.rg_settings);
+        mRgSettings = view.findViewById(R.id.rg_settings);
+        mRbSemester = view.findViewById(R.id.rb_semester);
     }
 
     @Override
     public void initData() {
         setListener();
         initFragment();
-        mFragmentChanger = new FragmentChanger(getChildFragmentManager(), R.id.fl_content);
-        mFragmentChanger.showFragment(SemesterFragment.class);
+        if (mFragmentChanger == null) {
+            // 如果mFragmentChanger不为null，则代表在onCreate方法中恢复过，系统会帮助我们显示对应的fragment
+            mFragmentChanger = new FragmentChanger(getChildFragmentManager(), R.id.fl_content);
+            // 不使用RadioGroup的check方法，会多次调用onCheckedChanged
+            // https://stackoverflow.com/questions/10263778/radiogroup-calls-oncheckchanged-three-times
+            mRbSemester.setChecked(true);
+        }
     }
 
     private void initFragment() {
         mFragmentArray = new SparseArray<>();
         mFragmentArray.put(R.id.rb_semester, SemesterFragment.class);
         mFragmentArray.put(R.id.rb_table, TableFragment.class);
-        mFragmentArray.put(R.id.rb_background, CourseManagementFragment.class);
+        mFragmentArray.put(R.id.rb_course_management, CourseManagementFragment.class);
         mFragmentArray.put(R.id.rb_more, MoreFragment.class);
     }
 
     private void setListener() {
-        rgSettings.setOnCheckedChangeListener(this);
-        int childCount = rgSettings.getChildCount();
+        mRgSettings.setOnCheckedChangeListener(this);
+        int childCount = mRgSettings.getChildCount();
         for (int i = 0; i < childCount; i++) {
-            ((RadioButton) rgSettings.getChildAt(i)).setOnCheckedChangeListener(this);
+            ((RadioButton) mRgSettings.getChildAt(i)).setOnCheckedChangeListener(this);
         }
     }
 
@@ -77,5 +93,11 @@ public class SettingsFragment extends BaseFragment implements RadioGroup.OnCheck
             return;
         }
         TableData.getInstance().refreshDataIfNeed();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        mFragmentChanger.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
     }
 }
